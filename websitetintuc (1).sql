8c70b06479511c68225b4e95d5da7bab2486e7dc
Your Name <you@example.com>
2023-06-09 16:34:11 +0700

    up

--- websitetintuc (1).sql
+++ websitetintuc (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 01, 2023 lúc 11:46 AM
+-- Thời gian đã tạo: Th6 09, 2023 lúc 11:33 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 7.3.30
 
@@ -29,11 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `binhluan` (
   `id` int(11) NOT NULL,
-  `tin_tuc_id` int(11) DEFAULT NULL,
-  `nguoi_dung_id` int(11) DEFAULT NULL,
-  `noi_dung` text DEFAULT NULL,
-  `ngayTao` datetime NOT NULL,
-  `ngayCapNhat` datetime NOT NULL
+  `tintuc_id` int(11) DEFAULT NULL,
+  `nguoidung_id` int(11) DEFAULT NULL,
+  `noidung` text DEFAULT NULL,
+  `ngay` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -44,10 +43,24 @@ CREATE TABLE `binhluan` (
 
 CREATE TABLE `chude` (
   `id` int(11) NOT NULL,
-  `ten` varchar(255) NOT NULL,
-  `ngayTao` datetime NOT NULL,
-  `ngayCapNhat` datetime NOT NULL
+  `ten` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `chude`
+--
+
+INSERT INTO `chude` (`id`, `ten`) VALUES
+(1, 'Thế giới'),
+(2, 'Chính trị'),
+(3, 'Kinh tế'),
+(4, 'Xã hội'),
+(5, 'Thể thao'),
+(6, 'Giải trí'),
+(7, 'Công nghệ'),
+(8, 'Khoa học'),
+(9, 'Sức khỏe'),
+(10, 'Môi trường');
 
 -- --------------------------------------------------------
 
@@ -57,11 +70,19 @@ CREATE TABLE `chude` (
 
 CREATE TABLE `gopy` (
   `id` int(11) NOT NULL,
-  `nguoi_dung_id` int(11) DEFAULT NULL,
-  `noi_dung` text DEFAULT NULL,
-  `ngayTao` datetime NOT NULL,
-  `ngayCapNhat` datetime NOT NULL
+  `hoten` varchar(500) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `tieude` varchar(500) NOT NULL,
+  `noidung` text DEFAULT NULL,
+  `ngay` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `gopy`
+--
+
+INSERT INTO `gopy` (`id`, `hoten`, `email`, `tieude`, `noidung`, `ngay`) VALUES
+(1, 'Nguyễn Văn An', 'an@gmail.com', 'Góp ý', 'abcadsadada', '2023-06-09 14:27:52');
 
 -- --------------------------------------------------------
 
@@ -71,12 +92,19 @@ CREATE TABLE `gopy` (
 
 CREATE TABLE `nguoidung` (
   `id` int(11) NOT NULL,
-  `ten_dang_nhap` varchar(255) NOT NULL,
-  `mat_khau` varchar(255) NOT NULL,
-  `vai_tro` enum('user','admin') NOT NULL DEFAULT 'user',
-  `ngayTao` datetime NOT NULL,
-  `ngayCapNhat` datetime NOT NULL
+  `hoten` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `taikhoan` varchar(255) NOT NULL,
+  `matkhau` varchar(255) NOT NULL,
+  `vaitro` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `nguoidung`
+--
+
+INSERT INTO `nguoidung` (`id`, `hoten`, `email`, `taikhoan`, `matkhau`, `vaitro`) VALUES
+(1, 'Lê Văn A', 'levana@gmail.com', 'levana', '123456', 2);
 
 -- --------------------------------------------------------
 
@@ -86,11 +114,12 @@ CREATE TABLE `nguoidung` (
 
 CREATE TABLE `tintuc` (
   `id` int(11) NOT NULL,
-  `chu_de_id` int(11) DEFAULT NULL,
-  `tieu_de` varchar(255) NOT NULL,
-  `noi_dung` text DEFAULT NULL,
-  `ngayTao` datetime NOT NULL,
-  `ngayCapNhat` datetime NOT NULL
+  `chude_id` int(11) DEFAULT NULL,
+  `tieude` varchar(255) NOT NULL,
+  `anh` varchar(500) NOT NULL,
+  `noidung` text DEFAULT NULL,
+  `ngay` datetime NOT NULL DEFAULT current_timestamp(),
+  `nguoidung_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -101,9 +130,7 @@ CREATE TABLE `tintuc` (
 -- Chỉ mục cho bảng `binhluan`
 --
 ALTER TABLE `binhluan`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `tin_tuc_id` (`tin_tuc_id`),
-  ADD KEY `nguoi_dung_id` (`nguoi_dung_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `chude`
@@ -115,8 +142,7 @@ ALTER TABLE `chude`
 -- Chỉ mục cho bảng `gopy`
 --
 ALTER TABLE `gopy`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `nguoi_dung_id` (`nguoi_dung_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `nguoidung`
@@ -128,8 +154,7 @@ ALTER TABLE `nguoidung`
 -- Chỉ mục cho bảng `tintuc`
 --
 ALTER TABLE `tintuc`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `chu_de_id` (`chu_de_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
@@ -145,48 +170,25 @@ ALTER TABLE `binhluan`
 -- AUTO_INCREMENT cho bảng `chude`
 --
 ALTER TABLE `chude`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `gopy`
 --
 ALTER TABLE `gopy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `nguoidung`
 --
 ALTER TABLE `nguoidung`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `tintuc`
 --
 ALTER TABLE `tintuc`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `binhluan`
---
-ALTER TABLE `binhluan`
-  ADD CONSTRAINT `binhluan_ibfk_1` FOREIGN KEY (`tin_tuc_id`) REFERENCES `tintuc` (`id`),
-  ADD CONSTRAINT `binhluan_ibfk_2` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoidung` (`id`);
-
---
--- Các ràng buộc cho bảng `gopy`
---
-ALTER TABLE `gopy`
-  ADD CONSTRAINT `gopy_ibfk_1` FOREIGN KEY (`nguoi_dung_id`) REFERENCES `nguoidung` (`id`);
-
---
--- Các ràng buộc cho bảng `tintuc`
---
-ALTER TABLE `tintuc`
-  ADD CONSTRAINT `tintuc_ibfk_1` FOREIGN KEY (`chu_de_id`) REFERENCES `chude` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
